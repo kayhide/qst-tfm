221a86840b52ef069bbeded278d8011b5612d1d6
Hideaki Kawai <kayhide@gmail.com>
2023-02-28 00:30:34 +0100

    Optimze 2params

--- src/one_qubit/two_params.py
+++ src/one_qubit/two_params.py
@@ -19,7 +19,6 @@ def plot(aqaoa, f, params=None):
     ax = axs[0, 0]
     ax.set_title("Error")
     ax.set_ylim(-0.05, 1.05)
-    # ax.set_ymargin(0.1)
     ax.plot(ts, result.expect[2])
 
     ax = axs[0, 1]
@@ -43,16 +42,57 @@ def plot(aqaoa, f, params=None):
 
     plt.subplots_adjust(hspace=0.30)
     plt.savefig(f)
+    fig.clf()
+    plt.close()
     logger.say(f"create: {f}")
 
 
-out = "output/one_qubit/2params"
-os.makedirs(out, exist_ok=True)
+def fixed_params():
+    out = "output/one_qubit/2params"
+    os.makedirs(out, exist_ok=True)
 
-for p2, p1 in itertools.product(np.linspace(0, 1, 6), repeat=2):
-    name = f"{int(100*p2):03d}_{int(100*p1):03d}"
-    schedule = Schedule([p1, p2])
-    aqaoa = Aqaoa(schedule.spline)
-    aqaoa.run(4)
-    f = f"{out}/{name}.svg"
-    plot(aqaoa, f, params=schedule.params)
+    for p2, p1 in itertools.product(np.linspace(0, 1, 6), repeat=2):
+        name = f"{int(100*p2):03d}_{int(100*p1):03d}"
+        schedule = Schedule([p1, p2])
+        aqaoa = Aqaoa(schedule.spline)
+        aqaoa.run(4)
+        f = f"{out}/{name}.svg"
+        plot(aqaoa, f, params=schedule.points)
+
+def optimizing_params():
+    out = "output/one_qubit/2params_optimizing"
+    os.makedirs(out, exist_ok=True)
+
+    max_iteration = 100
+    delta = 1e-1
+    learning_rate = 1.0
+    _, p1, p2, _ = np.linspace(0, 1, 4)
+    working_params = np.array([p1, p2])
+    print(p1, p2, delta)
+    for i in range(1, max_iteration + 1):
+        print(f"Iteration: {i}")
+        schedule = Schedule(working_params.tolist())
+        aqaoa = Aqaoa(schedule.spline)
+        aqaoa.run(4)
+        base = aqaoa.result.expect[2][-1].real
+        print(f"Params: {working_params}")
+        print(f"Error: {base}")
+
+        name = f"{i:03d}"
+        f = f"{out}/{name}.svg"
+        plot(aqaoa, f, params=schedule.points)
+
+        diffs = np.zeros(len(schedule.params))
+
+        for n, _ in enumerate(schedule.params):
+            params = np.copy(schedule.params)
+            params[n] += delta
+            aqaoa1 = Aqaoa(Schedule(params).spline)
+            aqaoa1.run(4)
+            x = aqaoa1.result.expect[2][-1].real
+            diffs[n] = x - base
+
+        working_params -= diffs
+
+
+optimizing_params()
--- src/schedule.py
+++ src/schedule.py
@@ -8,10 +8,11 @@ import scipy as sp
 class Schedule:
     def __init__(self, params):
         self.n = len(params)
-        self.params = [0.0, *params, 1.0]
+        self.params = params
+        self.points = [0.0, *params, 1.0]
         self.spline = CubicHermiteSpline(
-            np.linspace(0, 1, len(self.params)),
-            self.params,
-            np.repeat(0.0, len(self.params)),
+            np.linspace(0, 1, len(self.points)),
+            self.points,
+            np.repeat(0.0, len(self.points)),
             extrapolate=False,
         )
